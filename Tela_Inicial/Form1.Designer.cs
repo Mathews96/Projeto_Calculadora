22bdd721190e76e7bf28842331eaa9f1fdda7339
Mathews96 <mathewsfreire@gmail.com>
2022-04-13 22:51:17 -0300

    v1.0.1

--- Tela_Inicial/Form1.Designer.cs
+++ Tela_Inicial/Form1.Designer.cs
@@ -28,12 +28,198 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1SOMA = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button2SUBTRAÇÃO = new System.Windows.Forms.Button();
+            this.button3MULTIPLICAÇÃO = new System.Windows.Forms.Button();
+            this.button4DIVIDISÃO = new System.Windows.Forms.Button();
+            this.button5IGUAL = new System.Windows.Forms.Button();
+            this.button6NUMERO1 = new System.Windows.Forms.Button();
+            this.button7NUMERO2 = new System.Windows.Forms.Button();
+            this.button8NUMERO3 = new System.Windows.Forms.Button();
+            this.button9NUMERO6 = new System.Windows.Forms.Button();
+            this.button10NUMERO5 = new System.Windows.Forms.Button();
+            this.button11NUMERO4 = new System.Windows.Forms.Button();
+            this.button12NUMERO9 = new System.Windows.Forms.Button();
+            this.button13NUMERO8 = new System.Windows.Forms.Button();
+            this.button14NUMERO7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1SOMA
+            // 
+            this.button1SOMA.Location = new System.Drawing.Point(12, 131);
+            this.button1SOMA.Name = "button1SOMA";
+            this.button1SOMA.Size = new System.Drawing.Size(57, 24);
+            this.button1SOMA.TabIndex = 0;
+            this.button1SOMA.Text = "+";
+            this.button1SOMA.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(309, 113);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // button2SUBTRAÇÃO
+            // 
+            this.button2SUBTRAÇÃO.Location = new System.Drawing.Point(75, 131);
+            this.button2SUBTRAÇÃO.Name = "button2SUBTRAÇÃO";
+            this.button2SUBTRAÇÃO.Size = new System.Drawing.Size(57, 24);
+            this.button2SUBTRAÇÃO.TabIndex = 2;
+            this.button2SUBTRAÇÃO.Text = "-";
+            this.button2SUBTRAÇÃO.UseVisualStyleBackColor = true;
+            // 
+            // button3MULTIPLICAÇÃO
+            // 
+            this.button3MULTIPLICAÇÃO.Location = new System.Drawing.Point(138, 131);
+            this.button3MULTIPLICAÇÃO.Name = "button3MULTIPLICAÇÃO";
+            this.button3MULTIPLICAÇÃO.Size = new System.Drawing.Size(57, 24);
+            this.button3MULTIPLICAÇÃO.TabIndex = 3;
+            this.button3MULTIPLICAÇÃO.Text = "*";
+            this.button3MULTIPLICAÇÃO.UseVisualStyleBackColor = true;
+            // 
+            // button4DIVIDISÃO
+            // 
+            this.button4DIVIDISÃO.Location = new System.Drawing.Point(201, 131);
+            this.button4DIVIDISÃO.Name = "button4DIVIDISÃO";
+            this.button4DIVIDISÃO.Size = new System.Drawing.Size(57, 24);
+            this.button4DIVIDISÃO.TabIndex = 4;
+            this.button4DIVIDISÃO.Text = "/";
+            this.button4DIVIDISÃO.UseVisualStyleBackColor = true;
+            // 
+            // button5IGUAL
+            // 
+            this.button5IGUAL.Location = new System.Drawing.Point(264, 131);
+            this.button5IGUAL.Name = "button5IGUAL";
+            this.button5IGUAL.Size = new System.Drawing.Size(57, 24);
+            this.button5IGUAL.TabIndex = 5;
+            this.button5IGUAL.Text = "=";
+            this.button5IGUAL.UseVisualStyleBackColor = true;
+            // 
+            // button6NUMERO1
+            // 
+            this.button6NUMERO1.Location = new System.Drawing.Point(12, 161);
+            this.button6NUMERO1.Name = "button6NUMERO1";
+            this.button6NUMERO1.Size = new System.Drawing.Size(89, 68);
+            this.button6NUMERO1.TabIndex = 6;
+            this.button6NUMERO1.Text = "1";
+            this.button6NUMERO1.UseVisualStyleBackColor = true;
+            // 
+            // button7NUMERO2
+            // 
+            this.button7NUMERO2.Location = new System.Drawing.Point(124, 161);
+            this.button7NUMERO2.Name = "button7NUMERO2";
+            this.button7NUMERO2.Size = new System.Drawing.Size(89, 68);
+            this.button7NUMERO2.TabIndex = 7;
+            this.button7NUMERO2.Text = "2";
+            this.button7NUMERO2.UseVisualStyleBackColor = true;
+            // 
+            // button8NUMERO3
+            // 
+            this.button8NUMERO3.Location = new System.Drawing.Point(231, 161);
+            this.button8NUMERO3.Name = "button8NUMERO3";
+            this.button8NUMERO3.Size = new System.Drawing.Size(89, 68);
+            this.button8NUMERO3.TabIndex = 8;
+            this.button8NUMERO3.Text = "3";
+            this.button8NUMERO3.UseVisualStyleBackColor = true;
+            // 
+            // button9NUMERO6
+            // 
+            this.button9NUMERO6.Location = new System.Drawing.Point(231, 253);
+            this.button9NUMERO6.Name = "button9NUMERO6";
+            this.button9NUMERO6.Size = new System.Drawing.Size(89, 68);
+            this.button9NUMERO6.TabIndex = 11;
+            this.button9NUMERO6.Text = "6";
+            this.button9NUMERO6.UseVisualStyleBackColor = true;
+            // 
+            // button10NUMERO5
+            // 
+            this.button10NUMERO5.Location = new System.Drawing.Point(124, 253);
+            this.button10NUMERO5.Name = "button10NUMERO5";
+            this.button10NUMERO5.Size = new System.Drawing.Size(89, 68);
+            this.button10NUMERO5.TabIndex = 10;
+            this.button10NUMERO5.Text = "5";
+            this.button10NUMERO5.UseVisualStyleBackColor = true;
+            // 
+            // button11NUMERO4
+            // 
+            this.button11NUMERO4.Location = new System.Drawing.Point(12, 253);
+            this.button11NUMERO4.Name = "button11NUMERO4";
+            this.button11NUMERO4.Size = new System.Drawing.Size(89, 68);
+            this.button11NUMERO4.TabIndex = 9;
+            this.button11NUMERO4.Text = "4";
+            this.button11NUMERO4.UseVisualStyleBackColor = true;
+            // 
+            // button12NUMERO9
+            // 
+            this.button12NUMERO9.Location = new System.Drawing.Point(231, 339);
+            this.button12NUMERO9.Name = "button12NUMERO9";
+            this.button12NUMERO9.Size = new System.Drawing.Size(89, 68);
+            this.button12NUMERO9.TabIndex = 14;
+            this.button12NUMERO9.Text = "9";
+            this.button12NUMERO9.UseVisualStyleBackColor = true;
+            // 
+            // button13NUMERO8
+            // 
+            this.button13NUMERO8.Location = new System.Drawing.Point(124, 339);
+            this.button13NUMERO8.Name = "button13NUMERO8";
+            this.button13NUMERO8.Size = new System.Drawing.Size(89, 68);
+            this.button13NUMERO8.TabIndex = 13;
+            this.button13NUMERO8.Text = "8";
+            this.button13NUMERO8.UseVisualStyleBackColor = true;
+            // 
+            // button14NUMERO7
+            // 
+            this.button14NUMERO7.Location = new System.Drawing.Point(12, 339);
+            this.button14NUMERO7.Name = "button14NUMERO7";
+            this.button14NUMERO7.Size = new System.Drawing.Size(89, 68);
+            this.button14NUMERO7.TabIndex = 12;
+            this.button14NUMERO7.Text = "7";
+            this.button14NUMERO7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(332, 417);
+            this.Controls.Add(this.button12NUMERO9);
+            this.Controls.Add(this.button13NUMERO8);
+            this.Controls.Add(this.button14NUMERO7);
+            this.Controls.Add(this.button9NUMERO6);
+            this.Controls.Add(this.button10NUMERO5);
+            this.Controls.Add(this.button11NUMERO4);
+            this.Controls.Add(this.button8NUMERO3);
+            this.Controls.Add(this.button7NUMERO2);
+            this.Controls.Add(this.button6NUMERO1);
+            this.Controls.Add(this.button5IGUAL);
+            this.Controls.Add(this.button4DIVIDISÃO);
+            this.Controls.Add(this.button3MULTIPLICAÇÃO);
+            this.Controls.Add(this.button2SUBTRAÇÃO);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button1SOMA);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button button1SOMA;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button button2SUBTRAÇÃO;
+        private Button button3MULTIPLICAÇÃO;
+        private Button button4DIVIDISÃO;
+        private Button button5IGUAL;
+        private Button button6NUMERO1;
+        private Button button7NUMERO2;
+        private Button button8NUMERO3;
+        private Button button9NUMERO6;
+        private Button button10NUMERO5;
+        private Button button11NUMERO4;
+        private Button button12NUMERO9;
+        private Button button13NUMERO8;
+        private Button button14NUMERO7;
     }
 }
