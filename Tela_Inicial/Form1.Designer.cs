44547b3584c37a7ea96f3ca29cbe3b7ef837cb77
Mathews96 <mathewsfreire@gmail.com>
2022-06-17 22:22:08 -0300

    v1.0.2

--- Tela_Inicial/Form1.Designer.cs
+++ Tela_Inicial/Form1.Designer.cs
@@ -28,198 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1SOMA = new System.Windows.Forms.Button();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.button2SUBTRAÇÃO = new System.Windows.Forms.Button();
-            this.button3MULTIPLICAÇÃO = new System.Windows.Forms.Button();
-            this.button4DIVIDISÃO = new System.Windows.Forms.Button();
-            this.button5IGUAL = new System.Windows.Forms.Button();
-            this.button6NUMERO1 = new System.Windows.Forms.Button();
-            this.button7NUMERO2 = new System.Windows.Forms.Button();
-            this.button8NUMERO3 = new System.Windows.Forms.Button();
-            this.button9NUMERO6 = new System.Windows.Forms.Button();
-            this.button10NUMERO5 = new System.Windows.Forms.Button();
-            this.button11NUMERO4 = new System.Windows.Forms.Button();
-            this.button12NUMERO9 = new System.Windows.Forms.Button();
-            this.button13NUMERO8 = new System.Windows.Forms.Button();
-            this.button14NUMERO7 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSinal_Igual = new System.Windows.Forms.Button();
+            this.txtBxTela = new System.Windows.Forms.TextBox();
+            this.btnNumero_5 = new System.Windows.Forms.Button();
+            this.btnNumero_4 = new System.Windows.Forms.Button();
+            this.btnNumero_6 = new System.Windows.Forms.Button();
+            this.btnNumero_3 = new System.Windows.Forms.Button();
+            this.btnNumero_7 = new System.Windows.Forms.Button();
+            this.btnNumero_2 = new System.Windows.Forms.Button();
+            this.btnNumero_8 = new System.Windows.Forms.Button();
+            this.btnNumero_1 = new System.Windows.Forms.Button();
+            this.btnNumero_9 = new System.Windows.Forms.Button();
+            this.btnSinal_Divisao = new System.Windows.Forms.Button();
+            this.btnSinal_Multiplicacao = new System.Windows.Forms.Button();
+            this.lblOperacao = new System.Windows.Forms.Label();
+            this.btnSinal_Subtracao = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSinal_Adicao = new System.Windows.Forms.Button();
+            this.btnPonto = new System.Windows.Forms.Button();
+            this.btnNumero_0 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1SOMA
+            // btnSinal_Igual
             // 
-            this.button1SOMA.Location = new System.Drawing.Point(12, 131);
-            this.button1SOMA.Name = "button1SOMA";
-            this.button1SOMA.Size = new System.Drawing.Size(57, 24);
-            this.button1SOMA.TabIndex = 0;
-            this.button1SOMA.Text = "+";
-            this.button1SOMA.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.btnSinal_Igual, "btnSinal_Igual");
+            this.btnSinal_Igual.Name = "btnSinal_Igual";
+            this.btnSinal_Igual.UseVisualStyleBackColor = true;
+            this.btnSinal_Igual.Click += new System.EventHandler(this.clickTecla);
             // 
-            // flowLayoutPanel1
+            // txtBxTela
             // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(309, 113);
-            this.flowLayoutPanel1.TabIndex = 1;
+            resources.ApplyResources(this.txtBxTela, "txtBxTela");
+            this.txtBxTela.Name = "txtBxTela";
             // 
-            // button2SUBTRAÇÃO
+            // btnNumero_5
             // 
-            this.button2SUBTRAÇÃO.Location = new System.Drawing.Point(75, 131);
-            this.button2SUBTRAÇÃO.Name = "button2SUBTRAÇÃO";
-            this.button2SUBTRAÇÃO.Size = new System.Drawing.Size(57, 24);
-            this.button2SUBTRAÇÃO.TabIndex = 2;
-            this.button2SUBTRAÇÃO.Text = "-";
-            this.button2SUBTRAÇÃO.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.btnNumero_5, "btnNumero_5");
+            this.btnNumero_5.Name = "btnNumero_5";
+            this.btnNumero_5.UseVisualStyleBackColor = true;
+            this.btnNumero_5.Click += new System.EventHandler(this.clickTecla);
             // 
-            // button3MULTIPLICAÇÃO
+            // btnNumero_4
             // 
-            this.button3MULTIPLICAÇÃO.Location = new System.Drawing.Point(138, 131);
-            this.button3MULTIPLICAÇÃO.Name = "button3MULTIPLICAÇÃO";
-            this.button3MULTIPLICAÇÃO.Size = new System.Drawing.Size(57, 24);
-            this.button3MULTIPLICAÇÃO.TabIndex = 3;
-            this.button3MULTIPLICAÇÃO.Text = "*";
-            this.button3MULTIPLICAÇÃO.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.btnNumero_4, "btnNumero_4");
+            this.btnNumero_4.Name = "btnNumero_4";
+            this.btnNumero_4.UseVisualStyleBackColor = true;
+            this.btnNumero_4.Click += new System.EventHandler(this.clickTecla);
             // 
-            // button4DIVIDISÃO
+            // btnNumero_6
             // 
-            this.button4DIVIDISÃO.Location = new System.Drawing.Point(201, 131);
-            this.button4DIVIDISÃO.Name = "button4DIVIDISÃO";
-            this.button4DIVIDISÃO.Size = new System.Drawing.Size(57, 24);
-            this.button4DIVIDISÃO.TabIndex = 4;
-            this.button4DIVIDISÃO.Text = "/";
-            this.button4DIVIDISÃO.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.btnNumero_6, "btnNumero_6");
+            this.btnNumero_6.Name = "btnNumero_6";
+            this.btnNumero_6.UseVisualStyleBackColor = true;
+            this.btnNumero_6.Click += new System.EventHandler(this.clickTecla);
             // 
-            // button5IGUAL
+            // btnNumero_3
             // 
-            this.button5IGUAL.Location = new System.Drawing.Point(264, 131);
-            this.button5IGUAL.Name = "button5IGUAL";
-            this.button5IGUAL.Size = new System.Drawing.Size(57, 24);
-            this.button5IGUAL.TabIndex = 5;
-            this.button5IGUAL.Text = "=";
-            this.button5IGUAL.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.btnNumero_3, "btnNumero_3");
+            this.btnNumero_3.Name = "btnNumero_3";
+            this.btnNumero_3.UseVisualStyleBackColor = true;
+            this.btnNumero_3.Click += new System.EventHandler(this.clickTecla);
             // 
-            // button6NUMERO1
+            // btnNumero_7
             // 
-            this.button6NUMERO1.Location = new System.Drawing.Point(12, 161);
-            this.button6NUMERO1.Name = "button6NUMERO1";
-            this.button6NUMERO1.Size = new System.Drawing.Size(89, 68);
-            this.button6NUMERO1.TabIndex = 6;
-            this.button6NUMERO1.Text = "1";
-            this.button6NUMERO1.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.btnNumero_7, "btnNumero_7");
+            this.btnNumero_7.Name = "btnNumero_7";
+            this.btnNumero_7.UseVisualStyleBackColor = true;
+            this.btnNumero_7.Click += new System.EventHandler(this.clickTecla);
             // 
-            // button7NUMERO2
+            // btnNumero_2
             // 
-            this.button7NUMERO2.Location = new System.Drawing.Point(124, 161);
-            this.button7NUMERO2.Name = "button7NUMERO2";
-            this.button7NUMERO2.Size = new System.Drawing.Size(89, 68);
-            this.button7NUMERO2.TabIndex = 7;
-            this.button7NUMERO2.Text = "2";
-            this.button7NUMERO2.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.btnNumero_2, "btnNumero_2");
+            this.btnNumero_2.Name = "btnNumero_2";
+            this.btnNumero_2.UseVisualStyleBackColor = true;
+            this.btnNumero_2.Click += new System.EventHandler(this.clickTecla);
             // 
-            // button8NUMERO3
+            // btnNumero_8
             // 
-            this.button8NUMERO3.Location = new System.Drawing.Point(231, 161);
-            this.button8NUMERO3.Name = "button8NUMERO3";
-            this.button8NUMERO3.Size = new System.Drawing.Size(89, 68);
-            this.button8NUMERO3.TabIndex = 8;
-            this.button8NUMERO3.Text = "3";
-            this.button8NUMERO3.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.btnNumero_8, "btnNumero_8");
+            this.btnNumero_8.Name = "btnNumero_8";
+            this.btnNumero_8.UseVisualStyleBackColor = true;
+            this.btnNumero_8.Click += new System.EventHandler(this.clickTecla);
             // 
-            // button9NUMERO6
+            // btnNumero_1
             // 
-            this.button9NUMERO6.Location = new System.Drawing.Point(231, 253);
-            this.button9NUMERO6.Name = "button9NUMERO6";
-            this.button9NUMERO6.Size = new System.Drawing.Size(89, 68);
-            this.button9NUMERO6.TabIndex = 11;
-            this.button9NUMERO6.Text = "6";
-            this.button9NUMERO6.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.btnNumero_1, "btnNumero_1");
+            this.btnNumero_1.Name = "btnNumero_1";
+            this.btnNumero_1.UseVisualStyleBackColor = true;
+            this.btnNumero_1.Click += new System.EventHandler(this.clickTecla);
             // 
-            // button10NUMERO5
+            // btnNumero_9
             // 
-            this.button10NUMERO5.Location = new System.Drawing.Point(124, 253);
-            this.button10NUMERO5.Name = "button10NUMERO5";
-            this.button10NUMERO5.Size = new System.Drawing.Size(89, 68);
-            this.button10NUMERO5.TabIndex = 10;
-            this.button10NUMERO5.Text = "5";
-            this.button10NUMERO5.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.btnNumero_9, "btnNumero_9");
+            this.btnNumero_9.Name = "btnNumero_9";
+            this.btnNumero_9.UseVisualStyleBackColor = true;
+            this.btnNumero_9.Click += new System.EventHandler(this.clickTecla);
             // 
-            // button11NUMERO4
+            // btnSinal_Divisao
             // 
-            this.button11NUMERO4.Location = new System.Drawing.Point(12, 253);
-            this.button11NUMERO4.Name = "button11NUMERO4";
-            this.button11NUMERO4.Size = new System.Drawing.Size(89, 68);
-            this.button11NUMERO4.TabIndex = 9;
-            this.button11NUMERO4.Text = "4";
-            this.button11NUMERO4.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.btnSinal_Divisao, "btnSinal_Divisao");
+            this.btnSinal_Divisao.Name = "btnSinal_Divisao";
+            this.btnSinal_Divisao.UseVisualStyleBackColor = true;
+            this.btnSinal_Divisao.Click += new System.EventHandler(this.clickTecla);
             // 
-            // button12NUMERO9
+            // btnSinal_Multiplicacao
             // 
-            this.button12NUMERO9.Location = new System.Drawing.Point(231, 339);
-            this.button12NUMERO9.Name = "button12NUMERO9";
-            this.button12NUMERO9.Size = new System.Drawing.Size(89, 68);
-            this.button12NUMERO9.TabIndex = 14;
-            this.button12NUMERO9.Text = "9";
-            this.button12NUMERO9.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.btnSinal_Multiplicacao, "btnSinal_Multiplicacao");
+            this.btnSinal_Multiplicacao.Name = "btnSinal_Multiplicacao";
+            this.btnSinal_Multiplicacao.UseVisualStyleBackColor = true;
+            this.btnSinal_Multiplicacao.Click += new System.EventHandler(this.clickTecla);
             // 
-            // button13NUMERO8
+            // lblOperacao
             // 
-            this.button13NUMERO8.Location = new System.Drawing.Point(124, 339);
-            this.button13NUMERO8.Name = "button13NUMERO8";
-            this.button13NUMERO8.Size = new System.Drawing.Size(89, 68);
-            this.button13NUMERO8.TabIndex = 13;
-            this.button13NUMERO8.Text = "8";
-            this.button13NUMERO8.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.lblOperacao, "lblOperacao");
+            this.lblOperacao.BackColor = System.Drawing.Color.White;
+            this.lblOperacao.ForeColor = System.Drawing.Color.White;
+            this.lblOperacao.Name = "lblOperacao";
             // 
-            // button14NUMERO7
+            // btnSinal_Subtracao
             // 
-            this.button14NUMERO7.Location = new System.Drawing.Point(12, 339);
-            this.button14NUMERO7.Name = "button14NUMERO7";
-            this.button14NUMERO7.Size = new System.Drawing.Size(89, 68);
-            this.button14NUMERO7.TabIndex = 12;
-            this.button14NUMERO7.Text = "7";
-            this.button14NUMERO7.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.btnSinal_Subtracao, "btnSinal_Subtracao");
+            this.btnSinal_Subtracao.Name = "btnSinal_Subtracao";
+            this.btnSinal_Subtracao.UseVisualStyleBackColor = true;
+            this.btnSinal_Subtracao.Click += new System.EventHandler(this.clickTecla);
+            // 
+            // btnLimpar
+            // 
+            resources.ApplyResources(this.btnLimpar, "btnLimpar");
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.clickTecla);
+            // 
+            // btnSinal_Adicao
+            // 
+            resources.ApplyResources(this.btnSinal_Adicao, "btnSinal_Adicao");
+            this.btnSinal_Adicao.Name = "btnSinal_Adicao";
+            this.btnSinal_Adicao.UseVisualStyleBackColor = true;
+            this.btnSinal_Adicao.Click += new System.EventHandler(this.clickTecla);
+            // 
+            // btnPonto
+            // 
+            resources.ApplyResources(this.btnPonto, "btnPonto");
+            this.btnPonto.Name = "btnPonto";
+            this.btnPonto.UseVisualStyleBackColor = true;
+            this.btnPonto.Click += new System.EventHandler(this.clickTecla);
+            // 
+            // btnNumero_0
+            // 
+            resources.ApplyResources(this.btnNumero_0, "btnNumero_0");
+            this.btnNumero_0.Name = "btnNumero_0";
+            this.btnNumero_0.UseVisualStyleBackColor = true;
+            this.btnNumero_0.Click += new System.EventHandler(this.clickTecla);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(332, 417);
-            this.Controls.Add(this.button12NUMERO9);
-            this.Controls.Add(this.button13NUMERO8);
-            this.Controls.Add(this.button14NUMERO7);
-            this.Controls.Add(this.button9NUMERO6);
-            this.Controls.Add(this.button10NUMERO5);
-            this.Controls.Add(this.button11NUMERO4);
-            this.Controls.Add(this.button8NUMERO3);
-            this.Controls.Add(this.button7NUMERO2);
-            this.Controls.Add(this.button6NUMERO1);
-            this.Controls.Add(this.button5IGUAL);
-            this.Controls.Add(this.button4DIVIDISÃO);
-            this.Controls.Add(this.button3MULTIPLICAÇÃO);
-            this.Controls.Add(this.button2SUBTRAÇÃO);
-            this.Controls.Add(this.flowLayoutPanel1);
-            this.Controls.Add(this.button1SOMA);
+            this.Controls.Add(this.btnNumero_0);
+            this.Controls.Add(this.btnPonto);
+            this.Controls.Add(this.btnSinal_Adicao);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lblOperacao);
+            this.Controls.Add(this.btnSinal_Subtracao);
+            this.Controls.Add(this.btnNumero_5);
+            this.Controls.Add(this.btnNumero_4);
+            this.Controls.Add(this.btnSinal_Multiplicacao);
+            this.Controls.Add(this.btnNumero_6);
+            this.Controls.Add(this.txtBxTela);
+            this.Controls.Add(this.btnNumero_3);
+            this.Controls.Add(this.btnSinal_Divisao);
+            this.Controls.Add(this.btnNumero_7);
+            this.Controls.Add(this.btnSinal_Igual);
+            this.Controls.Add(this.btnNumero_2);
+            this.Controls.Add(this.btnNumero_9);
+            this.Controls.Add(this.btnNumero_8);
+            this.Controls.Add(this.btnNumero_1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private Button button1SOMA;
-        private FlowLayoutPanel flowLayoutPanel1;
-        private Button button2SUBTRAÇÃO;
-        private Button button3MULTIPLICAÇÃO;
-        private Button button4DIVIDISÃO;
-        private Button button5IGUAL;
-        private Button button6NUMERO1;
-        private Button button7NUMERO2;
-        private Button button8NUMERO3;
-        private Button button9NUMERO6;
-        private Button button10NUMERO5;
-        private Button button11NUMERO4;
-        private Button button12NUMERO9;
-        private Button button13NUMERO8;
-        private Button button14NUMERO7;
+        private Button btnSinal_Igual;
+        private TextBox txtBxTela;
+        private Button btnNumero_5;
+        private Button btnNumero_4;
+        private Button btnNumero_6;
+        private Button btnNumero_3;
+        private Button btnNumero_7;
+        private Button btnNumero_2;
+        private Button btnNumero_8;
+        private Button btnNumero_1;
+        private Button btnNumero_9;
+        private Button btnSinal_Divisao;
+        private Button btnSinal_Multiplicacao;
+        private Label lblOperacao;
+        private Button btnSinal_Subtracao;
+        private Button btnLimpar;
+        private Button btnSinal_Adicao;
+        private Button btnPonto;
+        private Button btnNumero_0;
     }
 }
