5bba277d3d6e7bb331382d6643ca8c6a62438288
Mathews96 <mathewsfreire@gmail.com>
2022-06-19 09:57:11 -0300

    v1.0.3

---
+++ Tela_Inicial/Calculadora.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*><><><><><><><> - [ BUGS -CONHECIDOS ] - <><><><><><><><*
+ * 1- Usuário consegue digitar quantas vírgulas quiser!   *
+ * Exemplo: ,,,,,,,,,,...                                 * 
+ * 2- Adicionar, quando casa decimal for = 0, mostrar     *
+ * somente o inteiro.                                     * 
+ *                                                        *
+ *><><><><><><><><><><><><><><><><><><><><><><><><><><><><*/
+namespace Tela_Inicial
+{
+    internal class Calculadora
+    {
+        public Calculadora()
+        {
+            valor2 = string.Empty;
+        }
+        public bool cliclouNoOperadorAritmetico { get; set; }
+        public string operadorAritmetico { get; set; }
+        public string valor1 { get; set; }
+        public string valor2 { get; set; }
+        public decimal total { get; set; }
+        public void limparCalculos()
+        {
+            this.cliclouNoOperadorAritmetico = false;
+            this.valor1 = "0";
+            this.valor2 = string.Empty;
+            this.total = 0;
+        }
+        public void setarValor(string valor)
+        {            
+            if (cliclouNoOperadorAritmetico == false)
+            {
+                this.valor1 += valor;
+            }
+            else if (cliclouNoOperadorAritmetico == true)
+            {
+                this.valor2 += valor;
+            }
+        }
+        public void setarTotal(string valor)
+        {
+            this.valor1 = valor;
+        }
+        public decimal obterTotal()
+        {
+            return this.total;
+        }
+        public void calcular(string sinalAritmetico, string valor1, string valor2)
+        {
+            switch(sinalAritmetico)
+            {
+                case "+":
+                    {
+                        this.total = string.IsNullOrEmpty(valor2) ? string.IsNullOrEmpty(valor1) ? 0 : decimal.Parse(valor1) : decimal.Parse(valor1) + decimal.Parse(valor2);
+                        setarTotal(this.total.ToString());
+                        this.valor2 = string.Empty;
+                        break;
+                    }
+                case "-":
+                    {
+                        this.total = string.IsNullOrEmpty(valor2) ? string.IsNullOrEmpty(valor1) ? 0 : decimal.Parse(valor1) : decimal.Parse(valor1) - decimal.Parse(valor2);
+                        setarTotal(this.total.ToString());
+                        this.valor2 = string.Empty;
+                        break;
+                    }
+                case "/":
+                    {
+                        this.total = string.IsNullOrEmpty(valor2) ? string.IsNullOrEmpty(valor1) ? 0 : decimal.Parse(valor1) : decimal.Parse(valor1) / decimal.Parse(valor2);
+                        setarTotal(this.total.ToString());
+                        this.valor2 = string.Empty;
+                        break;
+                    }
+                case "*":
+                    {
+                        this.total = string.IsNullOrEmpty(valor2) ? string.IsNullOrEmpty(valor1) ? 0 : decimal.Parse(valor1) : decimal.Parse(valor1) * decimal.Parse(valor2);
+                        setarTotal(this.total.ToString());
+                        this.valor2 = string.Empty;
+                        break;
+                    }
+                case "=":
+                    {
+                        if (string.IsNullOrEmpty(this.operadorAritmetico))
+                        {
+                            this.total = decimal.Parse(this.valor1);
+                            break;
+                        }
+                        this.calcular(this.operadorAritmetico, this.valor1, this.valor2);
+                        break;
+                    }            
+                default:
+                    {
+                        this.total = 3770;
+                        break;
+                    }
+            }
+        }
+    }
+}
--- Tela_Inicial/Form1.Designer.cs
+++ Tela_Inicial/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnSinal_Igual = new System.Windows.Forms.Button();
-            this.txtBxTela = new System.Windows.Forms.TextBox();
             this.btnNumero_5 = new System.Windows.Forms.Button();
             this.btnNumero_4 = new System.Windows.Forms.Button();
             this.btnNumero_6 = new System.Windows.Forms.Button();
@@ -48,6 +47,8 @@
             this.btnSinal_Adicao = new System.Windows.Forms.Button();
             this.btnPonto = new System.Windows.Forms.Button();
             this.btnNumero_0 = new System.Windows.Forms.Button();
+            this.lblOperador = new System.Windows.Forms.Label();
+            this.txtBxTela = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnSinal_Igual
@@ -55,89 +56,84 @@
             resources.ApplyResources(this.btnSinal_Igual, "btnSinal_Igual");
             this.btnSinal_Igual.Name = "btnSinal_Igual";
             this.btnSinal_Igual.UseVisualStyleBackColor = true;
-            this.btnSinal_Igual.Click += new System.EventHandler(this.clickTecla);
-            // 
-            // txtBxTela
-            // 
-            resources.ApplyResources(this.txtBxTela, "txtBxTela");
-            this.txtBxTela.Name = "txtBxTela";
+            this.btnSinal_Igual.Click += new System.EventHandler(this.clickTeclaSinal);
             // 
             // btnNumero_5
             // 
             resources.ApplyResources(this.btnNumero_5, "btnNumero_5");
             this.btnNumero_5.Name = "btnNumero_5";
             this.btnNumero_5.UseVisualStyleBackColor = true;
-            this.btnNumero_5.Click += new System.EventHandler(this.clickTecla);
+            this.btnNumero_5.Click += new System.EventHandler(this.clickTeclaNumerica);
             // 
             // btnNumero_4
             // 
             resources.ApplyResources(this.btnNumero_4, "btnNumero_4");
             this.btnNumero_4.Name = "btnNumero_4";
             this.btnNumero_4.UseVisualStyleBackColor = true;
-            this.btnNumero_4.Click += new System.EventHandler(this.clickTecla);
+            this.btnNumero_4.Click += new System.EventHandler(this.clickTeclaNumerica);
             // 
             // btnNumero_6
             // 
             resources.ApplyResources(this.btnNumero_6, "btnNumero_6");
             this.btnNumero_6.Name = "btnNumero_6";
             this.btnNumero_6.UseVisualStyleBackColor = true;
-            this.btnNumero_6.Click += new System.EventHandler(this.clickTecla);
+            this.btnNumero_6.Click += new System.EventHandler(this.clickTeclaNumerica);
             // 
             // btnNumero_3
             // 
             resources.ApplyResources(this.btnNumero_3, "btnNumero_3");
             this.btnNumero_3.Name = "btnNumero_3";
             this.btnNumero_3.UseVisualStyleBackColor = true;
-            this.btnNumero_3.Click += new System.EventHandler(this.clickTecla);
+            this.btnNumero_3.Click += new System.EventHandler(this.clickTeclaNumerica);
             // 
             // btnNumero_7
             // 
             resources.ApplyResources(this.btnNumero_7, "btnNumero_7");
             this.btnNumero_7.Name = "btnNumero_7";
             this.btnNumero_7.UseVisualStyleBackColor = true;
-            this.btnNumero_7.Click += new System.EventHandler(this.clickTecla);
+            this.btnNumero_7.Click += new System.EventHandler(this.clickTeclaNumerica);
             // 
             // btnNumero_2
             // 
             resources.ApplyResources(this.btnNumero_2, "btnNumero_2");
             this.btnNumero_2.Name = "btnNumero_2";
             this.btnNumero_2.UseVisualStyleBackColor = true;
-            this.btnNumero_2.Click += new System.EventHandler(this.clickTecla);
+            this.btnNumero_2.Click += new System.EventHandler(this.clickTeclaNumerica);
             // 
             // btnNumero_8
             // 
             resources.ApplyResources(this.btnNumero_8, "btnNumero_8");
             this.btnNumero_8.Name = "btnNumero_8";
             this.btnNumero_8.UseVisualStyleBackColor = true;
-            this.btnNumero_8.Click += new System.EventHandler(this.clickTecla);
+            this.btnNumero_8.Click += new System.EventHandler(this.clickTeclaNumerica);
             // 
             // btnNumero_1
             // 
             resources.ApplyResources(this.btnNumero_1, "btnNumero_1");
             this.btnNumero_1.Name = "btnNumero_1";
             this.btnNumero_1.UseVisualStyleBackColor = true;
-            this.btnNumero_1.Click += new System.EventHandler(this.clickTecla);
+            this.btnNumero_1.Click += new System.EventHandler(this.clickTeclaNumerica);
             // 
             // btnNumero_9
             // 
             resources.ApplyResources(this.btnNumero_9, "btnNumero_9");
             this.btnNumero_9.Name = "btnNumero_9";
             this.btnNumero_9.UseVisualStyleBackColor = true;
-            this.btnNumero_9.Click += new System.EventHandler(this.clickTecla);
+            this.btnNumero_9.Click += new System.EventHandler(this.clickTeclaNumerica);
             // 
             // btnSinal_Divisao
             // 
             resources.ApplyResources(this.btnSinal_Divisao, "btnSinal_Divisao");
             this.btnSinal_Divisao.Name = "btnSinal_Divisao";
             this.btnSinal_Divisao.UseVisualStyleBackColor = true;
-            this.btnSinal_Divisao.Click += new System.EventHandler(this.clickTecla);
+            this.btnSinal_Divisao.Click += new System.EventHandler(this.clickTeclaSinal);
             // 
             // btnSinal_Multiplicacao
             // 
             resources.ApplyResources(this.btnSinal_Multiplicacao, "btnSinal_Multiplicacao");
             this.btnSinal_Multiplicacao.Name = "btnSinal_Multiplicacao";
             this.btnSinal_Multiplicacao.UseVisualStyleBackColor = true;
-            this.btnSinal_Multiplicacao.Click += new System.EventHandler(this.clickTecla);
+            this.btnSinal_Multiplicacao.Click += new System.EventHandler(this.clickTeclaSinal);
             // 
             // lblOperacao
             // 
@@ -151,40 +147,53 @@
             resources.ApplyResources(this.btnSinal_Subtracao, "btnSinal_Subtracao");
             this.btnSinal_Subtracao.Name = "btnSinal_Subtracao";
             this.btnSinal_Subtracao.UseVisualStyleBackColor = true;
-            this.btnSinal_Subtracao.Click += new System.EventHandler(this.clickTecla);
+            this.btnSinal_Subtracao.Click += new System.EventHandler(this.clickTeclaSinal);
             // 
             // btnLimpar
             // 
             resources.ApplyResources(this.btnLimpar, "btnLimpar");
             this.btnLimpar.Name = "btnLimpar";
             this.btnLimpar.UseVisualStyleBackColor = true;
-            this.btnLimpar.Click += new System.EventHandler(this.clickTecla);
+            this.btnLimpar.Click += new System.EventHandler(this.clickTeclaC);
             // 
             // btnSinal_Adicao
             // 
             resources.ApplyResources(this.btnSinal_Adicao, "btnSinal_Adicao");
             this.btnSinal_Adicao.Name = "btnSinal_Adicao";
             this.btnSinal_Adicao.UseVisualStyleBackColor = true;
-            this.btnSinal_Adicao.Click += new System.EventHandler(this.clickTecla);
+            this.btnSinal_Adicao.Click += new System.EventHandler(this.clickTeclaSinal);
             // 
             // btnPonto
             // 
             resources.ApplyResources(this.btnPonto, "btnPonto");
             this.btnPonto.Name = "btnPonto";
             this.btnPonto.UseVisualStyleBackColor = true;
-            this.btnPonto.Click += new System.EventHandler(this.clickTecla);
+            this.btnPonto.Click += new System.EventHandler(this.clickTeclaNumerica);
             // 
             // btnNumero_0
             // 
             resources.ApplyResources(this.btnNumero_0, "btnNumero_0");
             this.btnNumero_0.Name = "btnNumero_0";
             this.btnNumero_0.UseVisualStyleBackColor = true;
-            this.btnNumero_0.Click += new System.EventHandler(this.clickTecla);
+            this.btnNumero_0.Click += new System.EventHandler(this.clickTeclaNumerica);
+            // 
+            // lblOperador
+            // 
+            resources.ApplyResources(this.lblOperador, "lblOperador");
+            this.lblOperador.BackColor = System.Drawing.Color.White;
+            this.lblOperador.Name = "lblOperador";
+            // 
+            // txtBxTela
+            // 
+            resources.ApplyResources(this.txtBxTela, "txtBxTela");
+            this.txtBxTela.BackColor = System.Drawing.Color.White;
+            this.txtBxTela.Name = "txtBxTela";
             // 
             // Form1
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblOperador);
             this.Controls.Add(this.btnNumero_0);
             this.Controls.Add(this.btnPonto);
             this.Controls.Add(this.btnSinal_Adicao);
@@ -215,7 +224,6 @@
         #endregion
 
         private Button btnSinal_Igual;
-        private TextBox txtBxTela;
         private Button btnNumero_5;
         private Button btnNumero_4;
         private Button btnNumero_6;
@@ -233,5 +241,7 @@
         private Button btnSinal_Adicao;
         private Button btnPonto;
         private Button btnNumero_0;
+        private Label lblOperador;
+        private TextBox txtBxTela;
     }
 }
